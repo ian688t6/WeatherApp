7bf1a0d090e41856d59356d9ce939409eea0f523
ian688t6 <gagovski@163.com>
2018-03-06 20:08:38 +0800

    Writing my first class

---
+++ app/src/main/java/com/kotlin/jyin/weatherapp/activities/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.kotlin.jyin.weatherapp.activities
+
+import adapters.ForecastListAdapter
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.kotlin.jyin.weatherapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val m_items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TAPPED IN WEATHERSTATION - 23/18",
+            "Sat 6/29 - Sunny - 20/7"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(m_items)
+    }
+}
