c721797815dbdf56f9e7370c9a3dacccc1a34266
ian <ian_yin@gcoreinc.com>
2018-03-07 14:26:23 +0800

    operator overloading

--- app/src/main/java/com/kotlin/jyin/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/kotlin/jyin/weatherapp/domain/model/DomainClasses.kt
@@ -3,5 +3,8 @@ package com.kotlin.jyin.weatherapp.domain.model
 /**
  * Created by jyin on 3/7/18.
  */
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+    fun size():Int = dailyForecast.size
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
--- app/src/main/java/com/kotlin/jyin/weatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/kotlin/jyin/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -11,10 +11,10 @@ class ForecastListAdapter(val weekForecast: ForecastList)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(TextView(parent.context))
 
-    override fun getItemCount(): Int = weekForecast.dailyForecast.size
+    override fun getItemCount(): Int = weekForecast.size()
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        with(weekForecast.dailyForecast[position]) {
+        with(weekForecast[position]) {
             holder.textView.text = "$date - $description - $high/$low"
         }
     }
