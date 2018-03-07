36aa1e2e5db25d82f8d52182d3bb455cef88236d
ian <ian_yin@gcoreinc.com>
2018-03-07 15:24:05 +0800

    add item_forecast layout

--- app/src/main/java/com/kotlin/jyin/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/kotlin/jyin/weatherapp/domain/model/DomainClasses.kt
@@ -7,4 +7,4 @@ data class ForecastList(val city: String, val country: String, val dailyForecast
     fun size():Int = dailyForecast.size
     operator fun get(position: Int): Forecast = dailyForecast[position]
 }
-data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
+data class Forecast(val date: String, val description: String, val high: Int, val low: Int, val iconUrl: String)
