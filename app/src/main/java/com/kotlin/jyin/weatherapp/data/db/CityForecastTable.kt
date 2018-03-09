bbeafbc34529afb4843804c163c0b34b05c4f2ab
ian688t6 <gagovski@163.com>
2018-03-09 10:10:32 +0800

    Creating an SQLiteOpenHelper

---
+++ app/src/main/java/com/kotlin/jyin/weatherapp/data/db/CityForecastTable.kt
@@ -0,0 +1,22 @@
+package com.kotlin.jyin.weatherapp.data.db
+
+/**
+ * Created by jyin on 3/9/18.
+ */
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
